a8940c3106fc4bbc03f159465e76e1d497569bdb
Yash Tomar <yashtomar@yashs-air.mynetworksettings.com>
2023-11-28 10:03:33 -0700

    Change dataset

--- python/divide.py
+++ python/divide.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 print(current_dir)
 
 current_dir = '../data/whill'
-g_colab_full_path='/cotent/darknet/data/whill'
+g_colab_full_path='/content/darknet/data/whill'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
@@ -16,11 +16,11 @@ file_train = open('whill-train.txt', 'w')
 file_test = open('whill-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter == index_test:
         counter = 1
-        file_test.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
+        file_test.write(g_colab_full_path + "/" + title + '.png' + "\n")
     else:
-        file_train.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
+        file_train.write(g_colab_full_path + "/" + title + '.png' + "\n")
         counter = counter + 1
