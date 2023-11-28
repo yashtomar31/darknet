bf7bf9dc3989caac53ab655db3af66676abcff99
Yash Tomar <yashtomar@yashs-air.mynetworksettings.com>
2023-11-28 13:42:24 -0700

    correcting config

--- python/divide.py
+++ python/divide.py
@@ -16,11 +16,11 @@ file_train = open('whill-train.txt', 'w')
 file_test = open('whill-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):  
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter == index_test:
         counter = 1
-        file_test.write(g_colab_full_path + "/" + title + '.png' + "\n")
+        file_test.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
     else:
-        file_train.write(g_colab_full_path + "/" + title + '.png' + "\n")
+        file_train.write(g_colab_full_path + "/" + title + '.jpg' + "\n")
         counter = counter + 1
